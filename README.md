f055a1de0bb43864638493317b9ef6ff5e42f748
i_mills <izaakmills@Izaaks-MacBook-Pro.local>
2018-10-21 19:41:11 -0400

    deleting extra stuff

--- README.md
+++ README.md
@@ -2,7 +2,7 @@
 
 ### This full-stack site will take in results from users' surveys, then compare their answers with those from other users. The app will then display the name and picture of the user with the best overall match.
 
-# see deployed Heroku version here [heroku link]
+### see deployed Heroku version here [heroku link]
 
 # Install Instructions
 
--- app/routing/apiRoutes.js
+++ app/routing/apiRoutes.js
@@ -13,63 +13,6 @@ module.exports = function (app) {
     let comparisonUserTotalScore = 0;
     let friendScores = [];
 
-    // ********************************************
-    app.post("/api/friends/errorout", function (req, res) {
-
-        // Store current user scores in array.
-        let curScores = req.body.scores;
-        let curScores2 = curScores.map((num) => { return parseInt(num) })
-        let currentUserScores = curScores2.reduce((a, b) => { return a + b })
-
-        console.log("line 22 : Current user scores: " + currentUserScores);
-
-        // Determine the user's most compatible friend, if there are actually friends in the database
-        console.log("line 28 : number of friends in DB: " + userData.length)
-
-        userData.forEach(function (user) {
-            console.log("User: " + user.name)
-
-            // Convert each user in db scores into an array of numbers.
-            var comparisonUserScores = user.scores;
-            console.log("line 35 : comparison user score is :" + user.scores)
-
-            // Find total difference between current user and each user.
-            comparisonUserTotalScore = calculateUserCompatibilityScore(currentUserScores, comparisonUserScores);
-
-            // Build up array of user compatibility scores.
-            friendScores.push(comparisonUserTotalScore);
-        })
-        // display current db users scores
-        console.log("Array of friend scores: " + friendScores);
-
-        let index = 0;
-        let value = friendScores[0];
-
-        //get index of user that score is lowest from useData array
-        for (let i = 0; i < friendScores.length; i++) {
-            console.log("Value of item in array: " + friendScores[i]);
-            if (friendScores[i] < value) {
-                value = friendScores[i];
-                index = i;
-            }
-        }
-
-        // get match
-        console.log("Best friend name: " + userData[index].name);
-
-        // Send best friend as a response so we can display in modal.
-        res.send(userData[index]);
-
-        // Push new user to database
-        userData.push(req.body)
-
-        console.log("number of friends in DB now: " + userData.length)
-
-    });
-    // };
-
-    let totalDifference = 0;
-
     // New Post Section
 
     app.post("/api/friends", function (req, res) {
